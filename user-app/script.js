273c878f7a9b1eeda258e6e8051709cf16d218c8
ROSTAND LELE <rostoinfocus@gmail.com>
2025-02-06 15:51:46 +0100

    tp user api

--- user-app/script.js
+++ user-app/script.js
@@ -16,10 +16,31 @@ const fetchUser = async () => {
  await  fetch("https://randomuser.me/api/?results=24")
     .then((res) => res.json())
     .then((data) => UserData =data.results)
-    console.log(UserData);    
+    // console.log(UserData);    
 }
 const userDisplay = async () =>{
  await fetchUser()
+
+ const dateParser = (date) =>{
+  const newDate = new Date(date)
+  .toLocaleDateString("fr-FR", {
+    year : "numeric",
+    month : "long",
+    day : "numeric",
+    hour:"numeric"
+  })
+  return newDate
+ }
+
+ const calDay = (date) =>{
+  const today = new Date()
+  const todayTimestamp = Date.parse(today)
+  
+  const timestamp = Date.parse(date)
+  // console.log((todayTimestamp - timestamp) /(1000*60*60*24));
+  
+  return Math.ceil((todayTimestamp - timestamp) /(1000*60*60*24))
+ }
   document.body.innerHTML = UserData.map(
       (user) => 
         ` 
@@ -27,8 +48,8 @@ const userDisplay = async () =>{
           <img src="${user.picture.large}" 
           alt=" La phode de ${user.name.last}"  />
           <h4>${user.name.first} </h4> 
-          <p> ${user.location.city} , ${user.dob.date}</p>
-          <i> Membre depuis ${user.registered.date} </i>
+          <p> ${user.location.city} , ${dateParser(user.dob.date)}</p>
+          <i> Membre depuis ${calDay(user.registered.date)} jours </i>
 
         </div>
         `
